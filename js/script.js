dbda8e11d700daf71ab440ecee4bc9bf0cfb4843
ShivaEslami97 <shivaslami@gmail.com>
2022-05-29 18:48:49 +0430

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 const darkModeBtn = document.querySelector('.theme-mode');
+const theme = window.localStorage.getItem("theme");
+if (theme === "dark") {
+    document.body.classList.add("dark");
+}
 darkModeBtn.addEventListener('click', () => {
     document.body.classList.toggle('dark');
+    if (theme === "dark") {
+        window.localStorage.setItem("theme", "light");
+    }
+    else {
+        window.localStorage.setItem("theme", "dark");
+    }
+
 });
